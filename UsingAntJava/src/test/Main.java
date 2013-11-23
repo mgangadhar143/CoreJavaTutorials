d5a34c7fb49eec38427f8b3dbc95ce8296c90a8a
matlasurya <matla.surya@gmail.com>
2013-11-23 10:46:46 +0530

    UsingAntJava

---
+++ UsingAntJava/src/test/Main.java
@@ -0,0 +1,12 @@
+package test;
+
+import com.akrantha.ANT.FirstANT;
+
+public class Main {
+	public static void main(String[] args)
+	{
+		FirstANT fn=new FirstANT();
+		System.out.println("The result is :"+fn.mul(5,10));
+	}
+
+}
